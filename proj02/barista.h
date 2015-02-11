f7ff8306698fde6b1c6164bc9a822965f20988c9
Zachary Rosenthal <rosent76@msu.edu>
2015-02-11 17:07:33 -0500

    kind of finished header file

--- proj02/barista.h
+++ proj02/barista.h
@@ -4,30 +4,38 @@
 using std::cin; using std::cout; using std::endl;
 #include<string>
 using std::string;
-
+#include<vector>
+using std::vector;
 class AbstractOrderList {
     private:
     protected:
         AbstractOrderList();
+    public:
+        getVector();
 }
 class OrangeJuiceOrderList:public AbstractOrderList {
     private:
+        vector<OrangeJuice*> OjVector;
     public:
         OrangeJuiceOrderList(vector<OrangeJuice*>);
 }
 class BubbleTeaList:public AbstractOrderList {
     private:
+        vector<BubbleTea*> BtVector;
     public:
         BubbleTeaList(vector<BubbleTea*>);
 }
 class Barista {
-
+    public:
+        virtual deliverDrinks(AbstractOrderList *) = 0
 }
 class CoolBarista:public Barista {
-
+    public:
+        deliverDrinks(AbstractOrderList *);
 }
 class NewbieBarista:public Barista {
-
+     public:
+        deliverDrinks(AbstractOrderList *);
 }
 
 #endif
